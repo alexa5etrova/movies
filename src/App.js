2c2d0c7ee112cb2458bd645aa6893123dd9d5c9e
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-04-08 13:33:26 +0300

    async await

--- src/App.js
+++ src/App.js
@@ -6,23 +6,36 @@ import "./App.css";
 function App() {
   const [filmData, setFilmData] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedData = data.results.map((movie) => {
-          return {
-            ...movie,
-            id: movie.episode_id,
-            releaseDate: movie.release_date,
-            openingText: movie.opening_crawl,
-          };
-        });
-        setFilmData(transformedData);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const transformedData = data.results.map((movie) => {
+      return {
+        ...movie,
+        id: movie.episode_id,
+        releaseDate: movie.release_date,
+        openingText: movie.opening_crawl,
+      };
+    });
+    setFilmData(transformedData);
   }
+  // function fetchMoviesHandler() {
+  //   fetch("https://swapi.dev/api/films")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedData = data.results.map((movie) => {
+  //         return {
+  //           ...movie,
+  //           id: movie.episode_id,
+  //           releaseDate: movie.release_date,
+  //           openingText: movie.opening_crawl,
+  //         };
+  //       });
+  //       setFilmData(transformedData);
+  //     });
+  // }
 
   return (
     <React.Fragment>
